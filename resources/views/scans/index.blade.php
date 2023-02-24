196ca3c9f927bb6906054193657e64bf99649ff8
Amjad Alarori <amjad.alarori@itonomy.nl>
2023-02-24 22:43:19 +0100

    table style

--- resources/views/scans/index.blade.php
+++ resources/views/scans/index.blade.php
@@ -3,23 +3,27 @@
 @section('content')
 
     <div class="flex items-center justify-center">
-        <table class="border-collapse border border-gray-200 mt-8">
-            <thead>
-            <tr class="bg-gray-100">
-                <th class="border border-gray-200 px-4 py-2 text-left">Scan ID</th>
-                <th class="border border-gray-200 px-4 py-2 text-left">Created At</th>
+        <table class="">
+            <thead class="">
+            <tr class="">
+                <th>Scan ID</th>
+                <th>Created At</th>
             </tr>
             </thead>
             <tbody>
             @foreach($scans as $scan)
-                <tr class="border border-gray-200">
-
-                    <th class="border border-gray-200 px-4 py-2 text-left">
-                        <a href="{{ url('/scans/'.$scan->id) }}" class="bg-indigo-500 hover:bg-indigo-700 text-white rounded px-4 py-2">See Scan {{ $scan->id }}</a>
-                    </th>
-                    <td class="border border-gray-200 px-4 py-2">{{ $scan->created_at }}</td>
+                <tr>
+                    <td class="td">{{ $scan->created_at }}</td>
+                    <td class="td">
+                <span class="float-right rounded-md bg-green-600/50 px-4 py-px text-xs font-semibold uppercase text-green-900 antialiased">
+                    <a href="{{ url('/scans/'.$scan->id) }}">See Scan {{ $scan->id }}</a>
+                </span>
+                    </td>
                 </tr>
             @endforeach
             </tbody>
         </table>
+
     </div>
+
+
