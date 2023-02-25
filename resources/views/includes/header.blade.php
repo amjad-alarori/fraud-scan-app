a91834fb9f4b9ba3e55284774f50fb5ea792270d
Amjad Alarori <amjad.alarori@itonomy.nl>
2023-02-25 03:05:06 +0100

    show scans button style

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,9 +6,9 @@
             </a>
             <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
                 <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-                    <li>
-                        <a href="{{ url('scans') }}" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Show Scans</a>
-                    </li>
+                    <a href="{{ url('scans') }}" class="flex items-center">
+                        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Show Scans</span>
+                    </a>
                 </ul>
             </div>
         </div>
